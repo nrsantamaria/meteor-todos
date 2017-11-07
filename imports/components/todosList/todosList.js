e96466bda4101429b9d9844a2e707881032ff83d
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-11-07 13:02:52 -0800

    add checkbox to html and js component

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -28,6 +28,17 @@ class TodosListCtrl {
     // Clear form
     this.newTask = '';
   }
+  setChecked(task) {
+    //set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+  removeTask(task){
+    Tasks.remove(task._id),
+  }
 }
 
 export default angular.module('todosList', [
